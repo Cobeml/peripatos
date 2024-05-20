22cee7ce25282d096b5ba46fe2d76de02cada6ab
Cobe Liu <liucobe@gmail.com>
2024-05-20 15:29:25 -0400

    progress

---
+++ src/components/ui/container-scroll-animation.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React, { useRef } from "react";
+import { useScroll, useTransform, motion, MotionValue } from "framer-motion";
+
+export const ContainerScroll = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const containerRef = useRef<any>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+  });
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
+  const scaleDimensions = () => {
+    return isMobile ? [0.7, 0.9] : [1.05, .95];
+  };
+
+  const rotate = useTransform(scrollYProgress, [0, 5], [0, 90]);
+  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
+  const translate = useTransform(scrollYProgress, [0, 2], [0, 100]);
+
+  return (
+    <div
+      className="flex items-center justify-center relative"
+      ref={containerRef}
+    >
+      <div
+        className="py-10 md:py-40 w-full relative"
+        style={{
+          perspective: "1000px",
+        }}
+      >
+        <Card rotate={rotate} translate={translate} scale={scale}>
+          {children}
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export const Card = ({
+  rotate,
+  scale,
+  translate,
+  children,
+}: {
+  rotate: MotionValue<number>;
+  scale: MotionValue<number>;
+  translate: MotionValue<number>;
+  children: React.ReactNode;
+}) => {
+  return (
+    <motion.div
+      style={{
+        rotateX: rotate,
+        scale,
+        translateY: translate,
+    }}
+      className="w-full"
+    >
+      <div className="h-full w-full overflow-hidden rounded-2xl">
+        {children}
+      </div>
+    </motion.div>
+  );
+};

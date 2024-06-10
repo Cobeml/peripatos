77c29f995038558dd7d4bfd6dfc94bef7ae42194
Cobe Liu <liucobe@gmail.com>
2024-06-09 21:50:21 -0400

    scroll test 15

--- src/components/ui/container-scroll-animation2.tsx
+++ src/components/ui/container-scroll-animation2.tsx
@@ -34,7 +34,7 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="flex items-center justify-center relative"
+      className="h-[60rem] md:h-[80rem] flex items-center justify-center relative"
       ref={containerRef}
     >
       <div
@@ -67,9 +67,9 @@ export const Card = ({
         rotateX: rotate,
         scale,
       }}
-      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+      className="w-full"
     >
-      <div className=" h-full w-full  overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4 ">
+      <div className=" h-full w-full overflow-hidden">
         {children}
       </div>
     </motion.div>

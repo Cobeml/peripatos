1bf349cb703ecaa91dabeb5fc68745b46d8c5a9d
Cobe Liu <liucobe@gmail.com>
2024-06-16 09:16:34 -0400

    naming conv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -433,7 +433,7 @@ export default function Home() {
       <div className="fixed top-0 left-0 w-full z-50 bg-black border-b-2 border-white py-2 px-4 rounded-md">
         <div className="flex items-center justify-between w-full"> {/* Flex container for alignment */}
           <a href="#" rel="noopener noreferrer" className="w-[2rem] lg:w-[4rem] h-auto">
-            <Image src="/peripa-logo.png" alt="Logo" className="mr-8 w-full" width={60} height={60}/>
+            <Image src="/logo.png" alt="Logo" className="mr-8 w-full" width={60} height={60}/>
           </a>
           <a href="#waiting-list" className="justify-end">
             Join Waiting List

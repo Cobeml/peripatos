3252296a72481687b490d2da69afb4e6fd6123ff
Cobe Liu <liucobe@gmail.com>
2024-08-09 17:02:26 -0400

    navbar updates

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,8 +3,16 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar: React.FC = () => {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { href: '/', label: 'Home' }
+  ];
+
+  
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-black border-b-2 border-white py-2 px-4 rounded-md">
       <div className="flex items-center justify-between w-full">
@@ -12,15 +20,17 @@ const Navbar: React.FC = () => {
           <Image src="/logo.png" alt="Logo" className="mr-8 w-full" width={60} height={60} />
         </Link>
         <div className="flex items-center space-x-4 w-full ml-8">
-          <Link href="/" className="hover:text-gray-300">
-            Home
-          </Link>
-          <Link href="/learn" className="hover:text-gray-300">
-            Learn
-          </Link>
-          <Link href="/teach" className="hover:text-gray-300">
-            Teach
-          </Link>
+          {navLinks.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`hover:text-gray-300 ${
+                pathname === link.href ? 'text-gray-400' : 'text-pink-100'
+              }`}
+            >
+              {link.label}
+            </Link>
+            ))}
         </div>
         <Link href="/#mailing-list" className="justify-end w-[12rem]">
           Join Mailing List
